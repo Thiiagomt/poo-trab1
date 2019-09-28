d5609bafa48110a079ca7d659e1b0acafe5b19da
Thiago <tmoraes505@gmail.com>
2019-09-28 18:54:16 -0300

    Incluindo arquivo faltava

---
+++ cmake-build-debug/CMakeFiles/trab1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trab1.dir/main.cpp.o"
+  "CMakeFiles/trab1.dir/ContaCorrente.cpp.o"
+  "CMakeFiles/trab1.dir/Cliente.cpp.o"
+  "trab1.pdb"
+  "trab1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
