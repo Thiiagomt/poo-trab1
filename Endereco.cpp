7998a761c964eb69e3fb4fb8f984fd9ed2aeff88
Felipe Tavoni <felipe.tavoni@catijr.com.br>
2019-10-30 11:59:47 -0300

    Classe Endereço Finalizada
    Classe de endereço finalizada. Deve substituir os structs

---
+++ Endereco.cpp
@@ -0,0 +1,92 @@
+//
+// Created by felipe on 20/09/2019.
+//
+
+#include  <iostream>
+#include "Endereco.h"
+using namespace std;
+
+// Construtor
+Endereco::Endereco() {
+    setRua();
+    setBairro();
+    setCidade();
+    setUf();
+    setCep();
+    setNumero();
+}
+
+// Destrutor
+Endereco::~Endereco() = default;
+
+// Imprime info da Endereco
+void Endereco::printEndereco() const {
+    cout << "Endereco: " << getRua() << ", " << getBairro() << ", " << ", " << getNumero() << 
+    "\nCEP: " + getCep() + ", " + getCidade() << ", " << getUf();
+}
+
+// Metodo toString
+string Endereco::toString() const {
+    string retorno;
+    ostringstream sEndereco;
+    sEndereco << this->rua << "/" << this->bairro << "/" << this->cidade << "/" << this->numero << "/" << this->cep <<
+    "/" << this->uf;
+    retorno = sEndereco.str();
+    return retorno;
+}
+
+// Setters
+void Endereco::setRua() {
+    cout << "Rua: ";
+    getline(cin, this->rua);
+}
+
+void Endereco::setBairro() {
+    cout << "Bairro: ";
+    getline(cin, this->bairro);
+}
+
+void Endereco::setCidade() {
+    cout << "Cidade: ";
+    getline(cin, this->cidade);
+}
+
+void Endereco::setUf() {
+    cout << "Uf: ";
+    getline(cin, this->uf);
+}
+
+void Endereco::setCep() {
+    cout << "CEP: ";
+    getline(cin, this->cep);
+}
+
+void Endereco::setNumero() {
+    cout << "Numero: ";
+    cin >> this->numero;
+}
+
+// Getters
+string Endereco::getRua() const {
+    return this->rua;
+}
+
+string Endereco::getBairro() const {
+    return this->bairro;
+}
+
+string Endereco::getCidade() const {
+    return this->cidade;
+}
+
+string Endereco::getUf() const {
+    return this->uf;
+}
+
+string Endereco::getCep() const {
+    return this->cep;
+}
+
+string Endereco::getNumero() const {
+    return this->numero;
+}
---
+++ Endereco.h
@@ -0,0 +1,52 @@
+//
+// Created by felipe on 24/10/2019.
+//
+
+#ifndef TRAB2_END_H
+#define TRAB2_END_H
+
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Endereco {
+public:
+    // Construtor sem parâmetros
+    Endereco();
+
+    // Destrutor
+    ~Endereco();
+
+    // Imprime info Endereco
+    void printEndereco() const;
+
+    // Valida Endereco e método toString
+    string toString() const;
+
+    // Setters
+    void setRua();
+    void setBairro();
+    void setCidade();
+    void setUf();
+    void setCep();
+    void setNumero();
+
+    // Getters
+    string getRua() const;
+    string getBairro() const;
+    string getCidade() const;
+    string getUf() const;
+    string getCep() const;
+    string getNumero() const;
+
+private:
+    string rua;
+    string bairro;
+    string cidade;
+    string uf;
+    string cep;
+    string numero;
+};
+
+#endif //TRAB2_END_H
